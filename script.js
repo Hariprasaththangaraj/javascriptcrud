59ca83d89a42138846a5114ad2e8473c753a556f
Hariprasaththangaraj <97735303+Hariprasaththangaraj@users.noreply.github.com>
2022-11-29 21:10:14 +0530

    Updated

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-var selectedRow= null;
-function onFormsubmit(){
+ var selectedRow= null;
+// function onFormsubmit(e){
 
+//     event.preventDefault();
+//     var fromData = readFormData();
+//     if(selectedRow === null){
+//         insertNewRecord(data);
+//     }
+//         else{
+
+//         }
+// }
+
+// //Retrive data 
+// function readFormData(){
+//     var fromData ={};
+//     fromData["Product Code"] =document.getElementById("Product Code").value;
+//     fromData["Product"] =document.getElementById("Product").value;
+//     fromData["Quality"] =document.getElementById("Quality").value;
+//     fromData["Product Price"] =document.getElementById("Product Price").value;
+//     return fromData;
+// }
+
+// //Insert Data
+// function insertNewRecord(data){
+//     var table=document.getElementById("StoreList").getElementsByTagName("tbody")[0];
+//     var newRow = table.insertRow(table.length);
+//     var cell1 = newRow.insertCell(0);
+//     cell1.innerHTML= data.productcode;
+//     var cell2 = newRow.insertCell(1);
+//     cell2.innerHTML= data.product;
+//     var cell3 = newRow.insertCell(2);
+//     cell3.innerHTML= data.Quality;
+//     var cell4 = newRow.insertCell(3);
+//     cell4.innerHTML= data.productprice;
+//     var cell5 = newRow.insertCell(4);
+//     cell5.innerHTML = "<button>Edit</button> <button>Delete</button>"
+//     }
+
+function onFormSubmit(){
+   var getFormData = readDataFromForm();
+   //To see we are getting value from the function
+   console.log(getFormData);
 }
 
-//Retrive data 
+function readDataFromForm(){
+    //To fetch the Values
+    var ProductCode=document.getElementById("ProductCode").value;
+    var Product=document.getElementById("Product").value;
+    var quality=document.getElementById("Quality").value;
+    var Productprice=document.getElementById("ProductPrice").value;
 
-function readFormData(){
-    var fromData ={};
-    fromData["Product Code"] =document.getElementById("Product Code").value;
-    fromData["Product"] =document.getElementById("Product").value;
-    fromData["Quality"] =document.getElementById("Quality").value;
-    fromData["Product Price"] =document.getElementById("Product Price").value;
-    return fromData;
+    // To store the value in a Single variable we need to create a array\
+    var arraydata = [ProductCode, Product, quality, Productprice];
+
+    //Then return the array
+    return arraydata;
 }
-
-//Insert Data
-function insertNewData(){
-    var table=document.getElementById("StoreList").getElementsByTagName("tbody")[0];
-    var newRow = table.insertRow(table.length);
-    }
